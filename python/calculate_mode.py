c5d5f0da3078955ef5b1bf8bd2740f18889394a1
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-05-30 17:10:12 -0500

    Python SOLVED

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,10 @@
-def calculate_mode():
-    pass
+def calculate_mode(array):
+    amount = {}
+    for i in array:
+        if i not in amount:
+            amount[i] = 1
+        else:
+            amount[i] += 1
+    result = [key for key, value in amount.items() if value == max(amount.values())]
+
+    return result
